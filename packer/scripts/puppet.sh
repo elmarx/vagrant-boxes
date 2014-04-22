c895d731b388b5526764f2963604980f76204897
Elmar Athmer <elmar@athmer.org>
2014-04-23 00:50:58 +0200

    download puppet via wget, and remove deb file after download

--- packer/scripts/puppet.sh
+++ packer/scripts/puppet.sh
@@ -1,6 +1,12 @@
 # Prepare puppetlabs repo
-wget http://apt.puppetlabs.com/puppetlabs-release-wheezy.deb
-dpkg -i puppetlabs-release-wheezy.deb
+DEB=puppetlabs-release-wheezy.deb
+
+cd /tmp
+wget http://apt.puppetlabs.com/$DEB \
+    -O /tmp -q
+
+dpkg -i /tmp/$DEB
+rm /tmp/$DEB
 apt-get update
 
 # Install puppet/facter

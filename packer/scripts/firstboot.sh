30678df802a27040a52bf29509a980cc48986f35
Elmar Athmer <elmar@athmer.org>
2014-04-23 07:17:22 +0200

    do not run firstboot script in background

--- packer/scripts/firstboot.sh
+++ packer/scripts/firstboot.sh
@@ -4,7 +4,7 @@ set -o nounset                              # Treat unset variables as an error
 set -o errexit
 
 
-sed -i s#"^exit 0$"#"test -f /var/tmp/firstboot.sh \&\& sh /var/tmp/firstboot.sh \&\n\nexit 0"# /etc/rc.local
+sed -i s#"^exit 0$"#"test -f /var/tmp/firstboot.sh \&\& sh /var/tmp/firstboot.sh\n\nexit 0"# /etc/rc.local
 
 cat > /var/tmp/firstboot.sh << EOF
 apt-get update > /dev/null
